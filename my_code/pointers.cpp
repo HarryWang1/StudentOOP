2d9f6543819ff490199d8071208c9b9d80da69e7
Hanyi Wang <hw1714@nyu.edu>
2019-06-04 11:41:07 -0400

    Update pointers.cpp

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -4,15 +4,53 @@
 using namespace std;
 
 void inc_by_value(int n) {
-    ++n;
+	++n;
 }
 
 
-void inc_with_pointer(int* n) {
-    ++(*n);
+void inc_with_pointer(int* iptr) {
+	++ iptr;
 }
 
 
 void inc_with_reference(int& n) {
-    ++n;
+	++n;
+}
+
+
+Thing** create_array_of_things(int n) {
+	Thing** ptr = new Thing*[n];
+	for(int i = 0; i < n; ++i){
+		ptr[i] = new Thing(i);
+	}
+    return nullptr;
+}
+
+void print_all_things(Thing** things, int n) {
+	for( int i = 0; i < n; ++i){
+		cout<< things[i]->value <<endl;
+	}
+}
+
+void double_all_things(Thing** things, int n) {
+	for(int i = 0; i < n; ++i){
+		things[i] -> value *= 2
+	}
+}
+
+void delete_all_things(Thing** things, int n) {
+	for (int i = 0; i < n; ++i){
+		delete things[i];
+	}
+}
+
+
+void assignTA(Student& s, Student& ta) {
+	s.ta = &ta
+}
+
+void printTAs(vector<Student> students) {
+	for(Student s : students){
+		cout << (s.ta) -> name << endl;
+	}
 }
