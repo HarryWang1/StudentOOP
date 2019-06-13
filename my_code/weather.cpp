2373646c710761b602b8bbb831aa56ebd2d20f87
Hanyi Wang <hw1714@nyu.edu>
2019-06-13 09:58:02 -0400

    ..

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -1,92 +1,53 @@
 #include <iostream>
 #include <string>
 #include "weather.h"
-#include "date.h"
 #include <vector>
 using namespace std;
 
-const double F_TO_C = 5/9;
-const double C_TO_F = 9/5;
-
-Image:: Image(int w, int h, std::string flnm): width(w), height(h),filename(flnm){}
+Image:: Image(int w, int h, std::string flnm): width(w), height(h),filename(flnm){
+    cout << "In Image::Image(int, int, string)\n";
+    image_buf = new char[image_sz()];
+}
 
 Image:: Image(const Image& img2){
-	delete[] image_buf;
-	image_buf = new char[image_sz()];
-	for(int i = 0; i < image_sz(); ++i){
-		image_buf[i] = img2.image_buf[i];
-	}
+    image_buf = new char[width*height];
+    for(int i = 0; i < image_sz(); ++i){
+        image_buf[i] = img2.image_buf[i];
+    }
 }
 
 Image:: ~Image(){
-	delete[] image_buf;
+    delete image_buf;
 }
 
 Image& Image:: operator=(const Image& img2){
-	if(&img2 != this){
-		delete[] image_buf;
-		image_buf = new char[image_sz()];
-		for(int i = 0; i < image_sz(); ++i){
-			image_buf[i] = img2.image_buf[i];
-		}
-	}
-	return *this;
+    if(&img2 != this){
+        delete image_buf;
+        image_buf = new char[width*height];
+        for(int i = 0; i < width*height; ++i){
+            image_buf[i] = img2.image_buf[i];
+        }
+    }
+    return *this;
 }
 
 int Image:: image_sz(){
-	return height*width;
+    return height*width;
 }
 
 void display(const std::string& s){
-	cout<< s << endl;
+    cout<< s << endl;
 }
 
-
-
-double WReading:: get_tempF(){
-	return ((temperature*C_TO_F)+32);
-}
-double WReading:: get_tempC(){
-	return ((temperature-32)*F_TO_C);
+void make_images1(Image& img1) {
+    for (int i = 0; i < 10; i++) {
+        Image img2 = Image(10, 10, "foo.txt");
+        img2 = img1;
+    }
 }
 
-/*
- * A constructor for weather class.
- * */
-Weather::Weather(std::string nm, GPS loc) : station_nm(nm), my_loc(loc) {}
-
-string Weather::get_name() const {
-    return station_nm;
-}
-
-int Weather:: get_rating() const{
-	return rating;
-}
-
-void Weather:: set_rating(int new_rating){
-	rating = new_rating;
-}
-
-void Weather:: add_reading(WReading wr){
-	wreadings.push_back(wr);
-}
-
-
-std:: ostream& operator<<(std::ostream& os, const GPS& gps){
-	os << gps.latitude << "," << gps.longitude <<endl;
-	return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const WReading& wr){
-	os << "date: " << wr.date << ", temp: " << wr.temperature << ", humi: " << wr.humidity  << ", wind: " << wr.windspeed <<endl;
-	return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const Weather& w){
-	os << "name: " << w.station_nm << "Rating: "<< w.rating <<"Loc: " << w.my_loc <<endl;
-	for(WReading wr : w.wreadings){
-		os << wr << " ";
-	}
-	os << endl;
-	return os;
+void make_images2(Image& img1) {
+    for (int i = 0; i < 10; i++) {
+        Image img2 = img1;
+    }
 }
--- tests/test_weather.cpp
+++ tests/test_weather.cpp
@@ -7,28 +7,66 @@
 
 using namespace std;
 
+/*void get_wreadings(string filenm, Weather& w) {
+    ifstream rfile(filenm);
+    if (!rfile) {
+        cout << "Could not read input file: " << filenm << endl;
+        exit(1);
+    }
+    int m, d, y;
+    double temp, hum, ws;
+    while (rfile >> m >> d >> y >> temp >> hum >> ws) {
+        WReading wr = WReading(Date(d, m, y), temp, hum, ws);
+        w.add_reading(wr);
+    }
+    rfile.close();
+}
+*/
 void make_images1(Image& img1) {
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 10; i++) {
         Image img2 = Image(10, 10, "foo.txt");
         img2 = img1;
     }
 }
 
 void make_images2(Image& img1) {
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 10; i++) {
         Image img2 = img1;
     }
 }
 
 
 int main() {
+    cout << "Hellooooooo"<<endl;
     string fnm = "happy.gif!";
-    Image img = Image(100, 100, fnm);
-    for (int i = 1; i < 100; i++) {
+    Image img = Image(10, 10, fnm);
+    for (int i = 1; i < 10; i++) {
          make_images1(img);
          make_images2(img);
     }
+    cout << "Goodbye"<<endl;
     exit(0);
+/*
+    Date date1 = Date(1, 1, 2019);
+    WReading test_temps = WReading(date1, 0, 50, 10);
+    int wr_size = sizeof(test_temps);
+    cout << "Size of a Date is " << sizeof(date1) << endl;
+    cout << "Size of a WReading is " << wr_size << endl;
+
+    double freezingF = test_temps.get_tempF();
+    assert(freezingF == 32.0);
+    test_temps = WReading(Date(1, 1, 2019), 100, 50, 10);
+    freezingF = test_temps.get_tempF();
+    assert(freezingF == 212.0);
+
+    Weather irkutsk = Weather("Irkutsk", GPS(46.3, 67.2));
+
+    string filenm;
+    cout << "Input the name of the new readings file:\n";
+    cin >> filenm;
+
+    get_wreadings(filenm, irkutsk);
 
 //    cout << irkutsk << endl;
+*/
 }
